e9d6a616aa83486669cdaca07b71c73d656387e7
Liam Ma <liam.jbr.ma@gmail.com>
2025-11-24 16:17:21 -0500

    Time: 67 ms (87.21%) | Memory: 20.5 MB (5.24%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -2,6 +2,8 @@ class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
         #keep track of how many ones currently while adding to number even if 0 as long the number of flipped is below the allowed amount. If it is above or equal need to reduce all on the left side until the number of flipped reduces. 
 
+        #it always increments it is just a matter of realizing when it is useing more than the allowed flips when you would reduce. 
+
         max_ones = 0
         curr = 0
         use_k = 0 
